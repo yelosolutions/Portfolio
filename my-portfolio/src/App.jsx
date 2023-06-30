8fb718eaf0265a2229640ffe365d17edc82ebbd6
jay <ronokamau@gmail.com>
2023-07-01 00:23:18 +0300

    figma resource migration

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+    return (
+        <div className="main">
+            <nav className="navbar">
+                <div className="yelo">
+                    Yelo
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default App
