74a347253be6b4d5e1ea6e53fc438d94e8bfa451
jay <ronokamau@gmail.com>
2023-07-05 22:36:36 +0300

    Contact section style update

--- my-portfolio/src/contact.jsx
+++ my-portfolio/src/contact.jsx
@@ -3,68 +3,64 @@ import React, {useState} from 'react'
 const Contact = () => {
     const [text, setText] = useState('');
     const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(text, email);
+        console.log(text, email, message);
         setEmail('');
         setText('');
+        setMessage('');
     };
     return (
-        <section id='contact' className='content-between gap-24 justify-center content-center text-center h-screen'>
-            <h2 className="section_title">
-                Contact
-            </h2>
-            <div className="contact">
-                <div className="contact_img">
-                    <img src="../assets/contact.png" alt="about pic" />
-                </div>
-                <form 
-                    action=""
-                    onSubmit={handleSubmit}
-                    className='flex flex-col items-center content-center justify-center gap-8 w-full p-2 rounded-lg bg-indigo-200 max-h-60'
-                >
-                    <div className="form">
-                        <div className='form_control'>
-                        <label 
-                            htmlFor="text"
-                            className='p-1'
-                        >
-                            Name
-                        </label>
+        <section id="contact" className="contact">
+            <div className="contact_img">
+                <img src="../assets/contact.png" alt="about pic" />
+            </div>
+            <div className="container mx-auto bg-indigo-50 rounded-sm shadow-md p-3">
+                <h2 className="text-3xl font-bold text-center mb-4">Contact Me</h2>
+                <p className="text-center text-indigo-600 mb-8">Kindly, feel free to reach out via email.</p>
+                <div className="max-w-md mx-auto">
+                    <p className="text-center text-lg font-semibold mb-4">Email: ronokamau@gmail.com</p>
+                    <form className="mb-4" onSubmit={handleSubmit}>
+                        <label className="label_style" htmlFor="name">Name</label>
                         <input 
-                            type="text"
-                            className='bg-indigo-100 rounded-sm w-full p-2'
+                            className="input_style" 
+                            type="text" 
+                            id="name"
                             placeholder='Enter name'
                             value={text}
                             onChange={(e) => setText(e.target.value)}
-                        />
-                        </div>
-                        <div className='form_control'>
-                            <label 
-                                htmlFor="email"
-                                className='p-1'
-                            >
-                                Email
-                            </label>
-                            <input 
-                                type="email"
-                                className='bg-indigo-100 rounded-sm w-full p-2'
-                                placeholder='Enter email'
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </div>
-                    </div>
-                    
-                    <button type='submit' className='btn'>
-                        <h4>Submit</h4>
-                    </button>
-                </form>
+                            name="name" 
+                            required/>
+                        <label className="label_style" htmlFor="email">Email</label>
+                        <input 
+                            className="input_style" 
+                            type="email" 
+                            id="email"
+                            placeholder='Enter email'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            name="email" 
+                            required/>
+                        <label className="label_style" htmlFor="message">Message</label>
+                        <textarea 
+                            className="text_area" 
+                            id="message"
+                            placeholder="What's on your mind?"
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                            name="message" 
+                            rows="4" 
+                            required
+                        >
+                        </textarea>
+                        <button className="btn" type="submit">Send Message</button>
+                    </form>
+                </div>
             </div>
-            
         </section>
     )
 }
 
-export default Contact
+export default Contact;
